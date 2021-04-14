708b88bc32d2faa195985347bae17c6a4ed32c28
Pavel Ershov <devnulpavel@gmail.com>
2021-04-14 14:04:38 +0300

    Config with inner Arc

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,21 +1,39 @@
+use std::{
+    sync::{
+        Arc
+    }
+};
 use derive_more::{
     Constructor
 };
 
-/// Внутренняя структура билдера запросов с неизменяемыми данными
 #[derive(Debug, Constructor)]
-pub struct PocketApiConfig { 
+pub struct Inner{
     pub http_client: reqwest::Client, // Arc inside
     pub api_url: reqwest::Url,
     pub api_consumer_key: String
 }
 
+/// Внутренняя структура билдера запросов с неизменяемыми данными
+#[derive(Debug, Clone)]
+pub struct PocketApiConfig { 
+    inner: Arc<Inner>
+}
 impl PocketApiConfig{
     pub fn new_default(http_client: reqwest::Client, api_consumer_key: String) -> PocketApiConfig {
-        PocketApiConfig{
+        let inner = Arc::new(Inner{
             http_client,
             api_consumer_key,
             api_url: reqwest::Url::parse("https://getpocket.com/").unwrap()
+        });
+        PocketApiConfig{
+            inner
         }
+    }
+}
+impl std::ops::Deref for PocketApiConfig{
+    type Target = Inner;
+    fn deref(&self) -> &Self::Target{
+        &self.inner
     }
 }
